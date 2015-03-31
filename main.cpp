1f3cdb2fe4c97c8c762ddc1197a559cf1dc6bb8c
Pascal <pascal.loth@tuhh.de>
2015-03-31 13:23:43 +0200

    krassss, nach 3 stunden finde ich dann endlich diesen verkackten fehler. wrap around die ganze zeit wegen des chars .... ist jetzt alles mal auf int geändert.

--- main.cpp
+++ main.cpp
@@ -9,55 +9,54 @@
 
 using namespace std;
 
-void edgeDetection(int k, cv::Mat *image, cv::Vec3b *p_color){
+void edgeDetection(int k, cv::Mat *image){
 
-  cv::Vec3b color = *p_color;
+  cv::Vec3b color;
+  color[0] = 0;
+  color[1] = 0;
+  color[2] = 255;
 
   std::vector<char> edge;
 
   int SCANLINE_SIZE = image->size().width;
-  char scanline[SCANLINE_SIZE];
+  int scanline[SCANLINE_SIZE];
 
-  char t_edge = 2 * T_EDGE;
-  char g_max = -t_edge;
-  char g_min = t_edge;
-  char x_peak = 0;
-  char fx_last = scanline[0];
-  char fx = 0;
-  char g = 0;
+  int t_edge = 2 * T_EDGE;
+  int g_max = -t_edge;
+  int g_min = t_edge;
+  int x_peak = 0;
+  int fx_last = scanline[0];
+  int fx = 0;
+  int g = 0;
 
-    for (int i=0; i<SCANLINE_SIZE; i++) {
-      scanline[i] = (image->at<cv::Vec3b>(k,i))[0];
+  for (int i=0; i<SCANLINE_SIZE; i++) {
+    scanline[i] = (image->at<cv::Vec3b>(k,i))[0];
+  }
+
+  for(int x = 2; x < SCANLINE_SIZE; x=x+2) {
+    fx = scanline[x];
+    g = fx - fx_last;
+    if(g > g_max) {
+      if(g_min < (-t_edge)) {
+        edge.push_back(x_peak);
+        image->at<cv::Vec3b>(k,x_peak) = color;
+      }
+      g_max = g;
+      g_min = t_edge;
+      x_peak = x - 1;
     }
 
-    for(int x = 2; x < SCANLINE_SIZE; x+=2) {
-      fx = scanline[x];
-      g = fx - fx_last;
-      if(g > g_max) {
-        if(g_min < (-t_edge)) {
-          edge.push_back(x_peak);
-           //Pixel färben
-          image->at<cv::Vec3b>(k,x_peak) = color;
-          printf("%d,%d\n",k,x_peak);
-        }
-        g_max = g;
-        g_min = t_edge;
-        x_peak = x - 1;
+    if(g < g_min) {
+      if(g_max > t_edge) {
+        edge.push_back(x_peak);
+        image->at<cv::Vec3b>(k,x_peak) = color;
       }
-
-      if(g < g_min) {
-        if(g_max > t_edge) {
-          edge.push_back(x_peak);
-          //Pixel färben
-          image->at<cv::Vec3b>(k,x_peak) = color;
-          printf("%d,%d\n",k,x_peak);
-        }
-        g_min = g;
-        g_max = (-t_edge);
-        x_peak = x - 1;
-      }
-      fx_last = fx;
+      g_min = g;
+      g_max = (-t_edge);
+      x_peak = x - 1;
     }
+    fx_last = fx;
+  }
 }
 
 
@@ -66,13 +65,8 @@ int main()
   cv::Mat image;
   image = cv::imread("bottom0007.png", CV_LOAD_IMAGE_COLOR);
 
-  cv::Vec3b color;
-  color[0] = 0;
-  color[1] = 0;
-  color[2] = 255;
-
   for (int k=0;k<image.size().height; k++){
-    edgeDetection(k, &image, &color);
+    edgeDetection(k, &image);
   }
 
   cv::namedWindow( "image", CV_WINDOW_AUTOSIZE );
